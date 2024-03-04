0ed05d8935dc4cba5a0642f8705c4f11adb1922b
unknown <singhrishikesh500@gmail.com>
2024-03-05 02:59:40 +0530

    improve ui

--- app/src/main/java/com/example/jetpack_compose/ui/activity/auth/LoginAuth.kt
+++ app/src/main/java/com/example/jetpack_compose/ui/activity/auth/LoginAuth.kt
@@ -1,6 +1,7 @@
 package com.example.jetpack_compose.ui.activity.auth
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
@@ -23,6 +26,10 @@ import androidx.compose.material.TextButton
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Password
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,10 +37,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
@@ -46,7 +59,11 @@ import com.example.jetpack_compose.utils.Constants.mpin
 import com.example.jetpack_compose.utils.Constants.signUpScreen
 
 @Composable
-fun LoginScreen(navController: NavController) {
+fun LoginScreen(
+    navController: NavController,
+    focusManager: FocusManager = LocalFocusManager.current,
+    keyboardController: SoftwareKeyboardController? = LocalSoftwareKeyboardController.current,
+) {
     // State for username and password
     var mobile by remember {
         mutableStateOf("")
@@ -75,27 +92,84 @@ fun LoginScreen(navController: NavController) {
         )
 
         Spacer(modifier = Modifier.height(30.dp))
+
         OutlinedTextField(
             value = mobile,
-            onValueChange = {
-                mobile = it
+            onValueChange = {newValue ->
+                // Check if the new value is less than or equal to 10 characters and only contains digits
+                if (newValue.length <= 10 && newValue.all { it.isDigit() }) {
+                    mobile = newValue
+                }
             },
-            label = { Text("Mobile") },
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
+            label = {
+                Text("Mobile")
+            },
+            modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                autoCorrect = true,
+                keyboardType = KeyboardType.Phone,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+                keyboardController?.hide()
+            }),
+            placeholder = { Text(text = "Enter your mobile no") },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Phone, contentDescription = "mobile")
+            },
+            trailingIcon = {
+                Icon(Icons.Default.Clear,
+                    contentDescription = "clear text",
+                    modifier = Modifier.clickable {
+
+                    })
+            },
+            shape = RoundedCornerShape(10.dp),
+            isError = false,
+            maxLines = 1,
+            singleLine = true
         )
-        Spacer(modifier = Modifier.height(16.dp))
+
+        Spacer(modifier = Modifier.height(6.dp))
 
         OutlinedTextField(
             value = password,
-            onValueChange = {
-                password = it
+            onValueChange = {newValue ->
+                // Check if the new value is less than or equal to 10 characters and only contains digits
+                if (newValue.length <= 5 && newValue.all { it.isDigit() }) {
+                    password = newValue
+                }
             },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth(),
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-        )
+            label = {
+                Text("Password")
+            },
+            modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                autoCorrect = true,
+                keyboardType = KeyboardType.Phone,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+                keyboardController?.hide()
+            }),
+            placeholder = { Text(text = "Enter your password") },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Password, contentDescription = "password")
+            },
+            trailingIcon = {
+                Icon(Icons.Default.Clear,
+                    contentDescription = "clear text",
+                    modifier = Modifier.clickable {
+                    })
+            },
+            shape = RoundedCornerShape(10.dp),
+            isError = false,
+            maxLines = 1,
+            singleLine = true,
+
+            )
 
         Spacer(modifier = Modifier.height(24.dp))
 
